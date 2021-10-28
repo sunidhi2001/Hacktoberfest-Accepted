251f13ff3945d0683c8db55fa19513a0ec052c30
MANISH SINGH TOMAR <72705246+MANISH-SINGH-TOMAR@users.noreply.github.com>
2021-10-28 19:41:46 +0530

    Update 14.cpp

--- C++/14.cpp
+++ C++/14.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 int main()
 {
-    int a=5,b=3;
+    int a,b;
+    cout<<"enter the value of a\n";
+    cin>>a;
+    cout<<"enter the value of b\n";
+    cin>>b;
     if(a>b)
     {
        cout<<"a is greater than b\n"<<endl;
-
-
     }
 else
     cout<<"b is greater "<<endl;
-
    return 0;
 }
